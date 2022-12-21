1e07b6907b9a3569b5c75d84714afaa42ee86ada
Emmanuelkiptum <91413986+Emmanuelkiptum@users.noreply.github.com>
2022-12-21 15:50:58 +0300

    Add files via upload

---
+++ 1. MY FIRST JAVA PROGRAM.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// This program is to  our first program application
+		System.out.println("Hello World!");
+		System.out.println("I really like Pizza!");
+        /*
+         * Our First program
+         * application has been
+         * created
+         */
+	}
+
+}
