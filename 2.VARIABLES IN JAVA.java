9ca35ec163424bf66684418d3b294b33442833d2
Emmanuelkiptum <91413986+Emmanuelkiptum@users.noreply.github.com>
2022-12-21 15:51:22 +0300

    Add files via upload

---
+++ 2.VARIABLES IN JAVA.java
@@ -0,0 +1,28 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		// VARIABLE especially on PRIMITIVE DATA TYPE
+	   boolean z = false;
+	   byte a = 127;
+	   short b = 130;
+       int x = 123;
+       long c = 134345565L;
+       float pie = 3.142f;
+       double decimal = 3.12377454565566;
+       char symbol = '@';
+       String name = "Kevin chumbaa";
+       
+       
+       System.out.println(z);
+       System.out.println(a);
+       System.out.println(b);
+       System.out.println(x);
+       System.out.println(c);
+       System.out.println(pie);
+       System.out.println(decimal);
+       System.out.println(symbol);
+       System.out.println("What is your name, "+ name);
+	}
+
+}
