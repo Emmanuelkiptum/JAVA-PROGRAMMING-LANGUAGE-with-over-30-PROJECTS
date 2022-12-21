a760822a4afa6357fa84b36de769a9b83fa037d4
Emmanuelkiptum <91413986+Emmanuelkiptum@users.noreply.github.com>
2022-12-21 15:54:56 +0300

    Add files via upload

---
+++ 9. DATATYPE_EXAMPLES.java
@@ -0,0 +1,17 @@
+
+
+package datatype_examples;
+
+public class DATATYPE_EXAMPLES {
+ 
+    public static void main(String[] args) {
+      
+        double x=9.3,y=10.7;
+        double sum =x+y;
+        double product =x*y;
+        double subtract = x-y;
+        System.out.println("The sum is:\n"+sum);
+        System.out.println("The product is:\n"+product);
+        System.out.println("The subtraction is:\n"+subtract);
+    }  
+}
